ecb22b96200b447c7713ac5a5d46ecd5a5ca9dcc
dabirrizvi <dabir@networldsports.co.uk>
2023-05-19 17:01:21 +0100

    routing

--- src/components/UI/Navbar/NavbarDesktop.vue
+++ src/components/UI/Navbar/NavbarDesktop.vue
@@ -1,23 +1,30 @@
 <template>
-    <div class="row desktop-header-row">
-        <div class="col-6 desktop-navbars">
+    <div class="row desktop-header-row" style="margin: auto;">
+        <div class="col-2 desktop-navbars">
             <span>Dabir Hasan Rizvi</span>
         </div>
         <div class="col-6 desktop-navbars">
             <!-- Routing -->
-            <router-link to="/home">
-                <span>Home</span>
+            <router-link to="/home" class="routing">
+                <span class="heading">Home</span>
             </router-link>
-            <router-link to="/projects">
-                <span>Projects</span>
+            <router-link to="/projects" class="routing">
+                <span class="heading">Projects</span>
             </router-link>
-            <router-link to="/certificates">
-                <span>Certificates</span>
+            <router-link to="/certificates" class="routing">
+                <span class="heading">Certificates</span>
             </router-link>
-            <router-link to="/contact">
-                <span>Contacts</span>
+            <router-link to="/contact" class="routing">
+                <span class="heading">Contacts</span>
             </router-link>
 
+        </div>
+        <div class="col-4 desktop-navbars" style="padding-left: 100px;">
+            <!-- LOGO -->
+            <span><i class="fa-brands fa-linkedin" style="font-size: 2rem; margin: auto; padding-left: 12px;"></i></span>
+            <span><i class="fa-solid fa-phone-volume social_links" style="font-size: 1.8rem; margin: auto"></i></span>
+            <span><i class="fa-solid fa-envelope social_links"
+                    style="font-size: 2rem; margin: auto; padding-right: 50px;"></i></span>
         </div>
     </div>
     <!-- router animation -->
@@ -35,14 +42,13 @@ export default {
 </script>
 
 <style lang="scss">
-.desktop-navbars{
+.desktop-navbars {
     display: flex;
     justify-content: space-between;
     margin: auto;
 }
-.desktop-header-row{
+
+.desktop-header-row {
     max-width: 1200px;
-    margin-left: auto;
-    margin-right: auto;
 }
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../components/Pages/Home/Home.vue'
-import Project from '../components/Pages/Projects/Project.vue'
+import Projects from '../components/Pages/Projects/Project.vue'
 import Certifcates from '../components/Pages/Certificates/Certificates.vue'
 import ContactUs from '../components/Pages/Contacts/Contact.vue'
 import NotFound from '../components/Pages/404.vue'
@@ -13,10 +13,10 @@ const router = createRouter({
     routes: [
         { path: '/', redirect: '/home' },
         { path: '/home', component: Home },
-        { path: '/projects', component: Project },
-        { path: '/certificates', component: Certifcates },
-        { path: '/contact', component: ContactUs },
-        { path: '/:notFound(.*)', component: NotFound }
+        { path: '/projects', component: Projects},
+        { path: '/certificates', component: Certifcates},
+        { path: '/contact', component: ContactUs},
+        { path: '/:notFound(.*)', component: NotFound}
     ],
 });
 
