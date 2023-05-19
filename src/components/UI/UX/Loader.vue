ea50bfc572e0e29bc20f3da6ee25d861eff73c4e
dabirrizvi <dabir@networldsports.co.uk>
2023-05-19 17:00:42 +0100

    added basecard and loader

---
+++ src/components/UI/UX/Loader.vue
@@ -0,0 +1,83 @@
+<template>
+    <div
+      :class="{ 'loader-display--visible': isVisible }"
+      id="loader"
+      class="flex-column"
+    >
+      <h2>Loading...</h2>
+      <SwappingSquaresSpinner
+        :animation-duration="2000"
+        :size="150"
+        color="rgb(8, 253, 216)"
+      />
+    </div>
+  </template>
+
+<script>
+  import { SwappingSquaresSpinner } from "epic-spinners";
+  export default {
+    name: "Loader",
+    components: {
+        SwappingSquaresSpinner
+    }
+  }
+</script>
+
+<script setup>
+import { computed } from "vue";
+const isVisible = computed(() => isLoading.value);
+</script>
+
+
+<style scoped>
+@media (max-width: 550px) {
+  h2 {
+    text-align: center;
+  }
+}
+h2 {
+  color: white;
+  padding: 10px;
+  font-size: 30px;
+  font-family: "Poppins";
+  letter-spacing: 1ch;
+}
+#loader {
+  background-color: #1d1d1d;
+  left: -100%;
+  bottom: 0;
+  right: 0;
+  top: 0;
+  position: fixed;
+  z-index: 9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation-duration: 0.8s;
+  animation-name: loader;
+  animation-fill-mode: forwards;
+}
+@keyframes loader {
+  100% {
+    left: 0;
+    bottom: 0;
+    right: 0;
+    top: 0;
+  }
+}
+#loader:not(.loader-display--visible) {
+  animation-duration: 1s;
+  animation-name: loaderEnd;
+  animation-fill-mode: forwards;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  top: 0;
+}
+@keyframes loaderEnd {
+  100% {
+    margin-left: 200%;
+    opacity: 0;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router/router.js'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import BaseCard from './components/UI/UX/BaseCard.vue'
+import Loader from './components/UI/UX/Loader.vue'
+
 const app = createApp(App)
+
+app.component('base-card', BaseCard);
+app.component('loader', Loader);
 
 app.use(router);
 router.isReady().then(function () {
