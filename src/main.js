5f28245f15b68787a45780c58d4292942d5ef048
dabirrizvi <dabir@networldsports.co.uk>
2023-05-20 17:08:36 +0100

    intitalised AOS

--- src/main.js
+++ src/main.js
@@ -3,15 +3,13 @@ import App from './App.vue'
 import router from './router/router.js'
 import 'bootstrap/dist/css/bootstrap.css'
 
-import BaseCard from './components/UI/UX/BaseCard.vue'
-import Loader from './components/UI/UX/Loader.vue'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-const app = createApp(App)
-
-app.component('base-card', BaseCard);
-app.component('loader', Loader);
-
+const app = createApp(App);
+AOS.init();
 app.use(router);
+
 router.isReady().then(function () {
     app.mount('#app')
 })
