edf458f6f44b069ea085d07df7dd6d17799f527b
dabirrizvi <dabir@networldsports.co.uk>
2023-05-21 14:03:05 +0100

    basic styling on background and headers

--- src/App.vue
+++ src/App.vue
@@ -11,24 +11,24 @@ import NavBar from "./components/UI/Navbar/NavbarSelector.vue"
 // To create parallax effect
 #app{
   background-image: url('./assets/background.jpg');
-   /* Full height */
-   height: fit-content; 
-    /* Create the parallax scrolling effect */
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
+  background-color: #f5ebe0;
+  //  /* Full height */
+    min-height: 110vh;
+    max-height:max-content;
+   /* Create the parallax scrolling effect */
+     background-attachment: fixed;
+     background-position: center;
+     background-repeat: no-repeat;
+     background-size: cover;
+     z-index: -999;
 }
+
 // section - background colours
 .container-section:nth-child(even) {
-  background: #ffd4a9;
+  background: #ffd4a9; 
 }
 
 // Headers
-.routing{
-  text-decoration: none;
-  color: black;
-}
 * {
   margin: 0;
   padding: 0rem;
