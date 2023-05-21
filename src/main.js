7f4334f85a04d4abd3a3d93bd06efc4fa7199916
dabirrizvi <dabir@networldsports.co.uk>
2023-05-21 14:03:24 +0100

    reduced the templates making it more dynamic

--- src/main.js
+++ src/main.js
@@ -4,10 +4,9 @@ import router from './router/router.js'
 import 'bootstrap/dist/css/bootstrap.css'
 
 // Page Templates
-import DescriptionImage from './components/UI/PageTemplate/DescriptionImageTemplate.vue'
-import ImageDescription from './components/UI/PageTemplate/ImageDescriptionTemplate.vue'
-import TwoImage from './components/UI/PageTemplate/TwoImageTemplate.vue'
-import TwoDescription from './components/UI/PageTemplate/TwoDescriptionTemplate.vue'
+
+import TwoColumns from './components/UI/PageTemplate/TwoColumn.vue'
+
 
 import AOS from 'aos'
 import 'aos/dist/aos.css'
@@ -16,10 +15,8 @@ const app = createApp(App);
 AOS.init();
 app.use(router);
 
-app.component('description-image', DescriptionImage);
-app.component('image-description',ImageDescription)
-app.component('two-image',TwoImage)
-app.component('two-description',TwoDescription)
+
+app.component('two-columns',TwoColumns)
 
 router.isReady().then(function () {
     app.mount('#app')
