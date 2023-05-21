fb1364476166e19eb6ba700be3b1fb1f3084a10f
dabirrizvi <dabir@networldsports.co.uk>
2023-05-21 14:05:33 +0100

    new template

---
+++ src/components/UI/PageTemplate/TwoColumn.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container-section">
+    <section class="content template-section">
+      <div
+        :class="firstDescriptionClass"
+        :data-aos="firstDescriptionAos"
+        data-aos-ease="ease"
+        data-aos-duration="1500"
+        data-aos-delay="500"
+      >
+        <slot name="firstDescription"></slot>
+      </div>
+      <div
+        :class="secondDescriptionClass"
+        :data-aos="secondDescriptionAos"
+        data-aos-ease="ease"
+        data-aos-duration="1500"
+        data-aos-delay="500"
+      >
+        <slot name="secondDescription"></slot>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    firstDescriptionAos: {
+      type: String,
+      required: true,
+    },
+    firstDescriptionClass: {
+      type: String,
+      default: '',
+    },
+    secondDescriptionAos: {
+      type: String,
+      required: true,
+    },
+    secondDescriptionClass: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+<style scoped>
+.template-section {
+  display: flex;
+  justify-content: space-evenly;
+  height: auto;
+  overflow: hidden;
+}
+
+@media screen and (max-width: 990px) {
+  .template-section {
+    display: block;
+    padding-top: 10px;
+    height: auto;
+  }
+}
+</style>
