ed79d3b9c9ea5acd5ea1128ba921cbab2b356d0b
dabirrizvi <dabir@networldsports.co.uk>
2023-06-06 11:14:23 +0100

    home page animation

--- src/components/Pages/Home/Home.vue
+++ src/components/Pages/Home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content"><h1>Home</h1></div>
+  <div data-aos="fade-in" data-aos-ease="ease" data-aos-duration="1500" class="content"><h1>Home</h1></div>
   <!-- test -->
 </template>
 
