9fe893a906e5fc3d43b59c5efc2abf2f59ee8d6b
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-06-15 22:36:28 +0100

    comments

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { createApp, isMemoSame } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
+// Importing Bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap'
 
 // Page Templates
-
 import TwoColumns from './components/UI/PageTemplate/TwoColumn.vue'
 import Timeline from './components/UI/PageTemplate/Timeline.vue'
 
-
+// Importing Animation on Scroll
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -17,7 +17,7 @@ const app = createApp(App);
 AOS.init();
 app.use(router);
 
-
+// Initiliasing templates
 app.component('two-columns',TwoColumns)
 app.component('timeline',Timeline)
 
