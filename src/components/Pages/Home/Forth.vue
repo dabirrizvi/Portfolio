b43f55f0ae7c6433ba9592e2f74acae670a1d91f
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-06-16 13:16:02 +0100

    added certificate page on home page

---
+++ src/components/Pages/Home/Forth.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="background-certificate">
+        <div class="certificate-template" data-aos="fade-left" data-aos-duration="1000" data-aos-easing="ease" data-aos-delay="500">
+            <swiper :effect="'coverflow'" :grabCursor="true" :centeredSlides="true" :slidesPerView="'auto'"
+                :coverflowEffect="{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }" :pagination="true" :navigation="true" :modules="modules" class="mySwiper" :observer="true">
+                <swiper-slide><img src="../../../assets/Certificates/devops.jpg" /></swiper-slide>
+                <swiper-slide><img src="../../../assets/Certificates/vue.jpg" /></swiper-slide>
+                <swiper-slide><img src="../../../assets/Certificates/ML.jpg" /></swiper-slide>
+                <swiper-slide><img src="../../../assets/Certificates/Deeplearning.jpg" /></swiper-slide>
+                <swiper-slide><img src="../../../assets/Certificates/AI.jpg" /></swiper-slide>
+                <swiper-slide><img src="../../../assets/Certificates/GameDev.jpg" /></swiper-slide>
+                <swiper-slide><img src="../../../assets/Certificates/c.jpg" /></swiper-slide>
+            </swiper>
+        </div>
+    </div>
+</template>
+<script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectCoverflow, Pagination, Navigation } from "swiper";
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        const modules = [EffectCoverflow, Pagination, Navigation];
+        return {
+            modules,
+        };
+    },
+};
+</script>
+<style scoped>
+.background-certificate {
+    background-color: #28282B;
+}
+.certificate-template{
+    align-items: center;
+}
+</style>
--- src/components/Pages/Home/Home.vue
+++ src/components/Pages/Home/Home.vue
@@ -2,14 +2,69 @@
 <First />
 <Second />
 <Third />
+<Fourth />
 </template>
 
 <script setup>
 import First from "./First.vue"
 import Second from "./Second.vue"
 import Third from "./Third.vue"
+import Fourth from "./Forth.vue"
 </script>
 
 <style>
+.swiper-button-disabled{
+    display: none;
+}
+.swiper {
+    width: 100%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+}
 
+.swiper-slide {
+    background-position: center;
+    background-size: cover;
+    width: 600px;
+
+}
+.swiper-pagination-bullet{
+    background: #8d99ae ;
+}
+.swiper-pagination-bullet-active{
+    background: white ;
+    border: 1px solid black;
+}
+.swiper-button-next, .swiper-button-prev{
+    color: bisque;
+    border: 3px solid;
+    border-radius: 50px;
+}
+.swiper-button-next:hover, .swiper-button-prev:hover{
+background-color: bisque;
+color: white;
+}
+
+.swiper-button-next:after, .swiper-button-prev:after{
+  font-size: 10px ;
+}
+.swiper-slide img {
+    display: block;
+    width: 600px;
+}
+  /* Responsive Styles */
+  @media (max-width: 990px) {
+    .swiper-slide {
+      width: auto;
+      max-width: 90vw;
+    }
+
+    .swiper-slide img {
+      width: 100%;
+      height: auto;
+    }
+  }
+ .swiper-button-next.swiper-button-disabled, .swiper-button-prev.swiper-button-disabled {
+    opacity: 0 !important;
+  }
 </style>
