0b4ff1b0015fb19214fe0b3989f6cd6be554da37
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-06-19 15:51:02 +0100

    added images for skills

--- src/components/Pages/Home/Skills.vue
+++ src/components/Pages/Home/Skills.vue
@@ -38,31 +38,31 @@
 
           {
             images: [
-              { src: 'https://cdn4.iconfinder.com/data/icons/scripting-and-programming-languages/512/js-512.png', color: '#ffd100' },
-              { src: 'https://cdn.freebiesupply.com/logos/thumbs/2x/java-logo.png', color: '#fff' },
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png', color: '#bbdefb' },
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png', color: '#0fa3b1' },
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/926px-C_Programming_Language.svg.png', color: '#fff3b0' },
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/991px-R_logo.svg.png', color: '#c9ada7' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Lang/JS.svg', color: '#ffd100' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Lang/JAVA.svg', color: '#fff' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Lang/Python.svg', color: '#bbdefb' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Lang/C.svg', color: '#0fa3b1' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Lang/C++.svg', color: '#fff3b0' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Lang/R.svg', color: '#c9ada7' },
             ],
           },
           {
             images: [
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png', color: '#fff3b0' },
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png', color: '#9ef01a' },
-              { src: 'https://www.svgrepo.com/download/21671/css.svg', color: '#bbdefb' },
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/2560px-Sass_Logo_Color.svg.png', color: '#0fa3b1' },
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Wordpress-Logo.svg/2048px-Wordpress-Logo.svg.png', color: '#fff3b0' },
-              { src: 'https://www.svgrepo.com/show/303592/magento-2-logo.svg', color: '#c9ada7' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Soft/', color: '#fff3b0' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Soft/', color: '#9ef01a' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Soft/', color: '#bbdefb' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Soft/', color: '#0fa3b1' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Soft/', color: '#fff3b0' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Soft/', color: '#c9ada7' },
             ],
           },
           {
             images: [
-              { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png', color: '#ddc49e' },
-              { src: 'https://icons-for-free.com/iconfiles/png/512/postgresql+plain+wordmark-1324760555518154961.png', color: '#fff3b0' },
-              { src: 'https://robotwealth.com/wp-content/uploads/2017/07/google-cloud-platform.png', color: '#9ef01a' },
-              { src: 'https://img.uxwing.com/wp-content/themes/uxwing/download/internet-network-technology/artificial-intelligence-ai-icon.png', color: '#bbdefb' },
-              { src: 'https://us.123rf.com/450wm/nexusby/nexusby2201/nexusby220100009/181526358-agile-development-method-vector-illustration.jpg', color: '#fff' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Misc/', color: '#ddc49e' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Misc/', color: '#fff3b0' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Misc/', color: '#9ef01a' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Misc/', color: '#bbdefb' },
+              { src: 'https://raw.githubusercontent.com/dabirrizvi/Portfolio/main/src/assets/Skills/Misc/', color: '#fff' },
               
             ],
           },
