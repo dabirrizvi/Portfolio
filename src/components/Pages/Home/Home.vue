e365bc78ab61e8a2b549e52972e9fa88c94983ee
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-06-22 10:33:34 +0100

    changed bullet styling

--- src/components/Pages/Home/Home.vue
+++ src/components/Pages/Home/Home.vue
@@ -1,10 +1,10 @@
 <template>
-<Intro />
-<Career />
-<Skills />
-<Certificates />
-<References />
-<Footer />
+  <Intro />
+  <Career />
+  <Skills />
+  <Certificates />
+  <References />
+  <Footer />
 </template>
 
 <script setup>
@@ -17,69 +17,83 @@ import Footer from "../../UI/PageTemplate/Footer.vue"
 </script>
 
 <style>
-.swiper-button-disabled{
-    display: none;
+.swiper-button-disabled {
+  display: none;
 }
+
 .swiper {
-    width: 100%;
-    padding-top: 50px;
-    padding-bottom: 50px;
+  width: 100%;
+  padding-top: 50px;
+  padding-bottom: 50px;
 }
 
 .swiper-slide {
-    background-position: center;
-    background-size: cover;
-    width: 600px;
+  background-position: center;
+  background-size: cover;
+  width: 600px;
 
 }
-.swiper-pagination-bullet{
-    background: #8d99ae ;
-}
-.swiper-pagination-bullet-active{
-    background: white ;
-    border: 1px solid #253237;
-}
-.swiper-button-next, .swiper-button-prev{
-    color: #253237;
-    border: 2px solid #283618;
-    border-radius: 50px;
-    opacity: 0.8;
-}
-.swiper-button-next:hover, .swiper-button-prev:hover{
-background-color: #253237;
-color: white;
-border: 2px solid white;
+
+.swiper-pagination-bullet {
+  background: #8d99ae;
 }
 
-.swiper-button-next:after, .swiper-button-prev:after{
-  font-size: 10px ;
+.swiper-pagination-bullet-active {
+  background: white;
+  border: 1px solid #253237;
 }
+
+.swiper-button-next,
+.swiper-button-prev {
+  border-radius: 50px;
+  opacity: 0.6;
+  background-color: #253237;
+  color: white;
+  border: 2px solid white;
+}
+
+.swiper-button-next:hover,
+.swiper-button-prev:hover {
+  opacity: 1;
+}
+
+.swiper-button-next:after,
+.swiper-button-prev:after {
+  font-size: 10px;
+}
+
 .swiper-slide img {
-    display: block;
-    width: 600px;
+  display: block;
+  width: 600px;
 }
-  /* Responsive Styles */
-  @media (max-width: 990px) {
-    .swiper-slide {
-      width: auto;
-      max-width: 90vw;
-    }
 
-    .swiper-slide img {
-      width: 100%;
-      height: auto;
-    }
-    .swiper-button-next, .swiper-button-prev{
-      display: none;
-    }
+/* Responsive Styles */
+@media (max-width: 990px) {
+  .swiper-slide {
+    width: auto;
+    max-width: 90vw;
   }
- .swiper-button-next.swiper-button-disabled, .swiper-button-prev.swiper-button-disabled {
-    opacity: 0 !important;
+
+  .swiper-slide img {
+    width: 100%;
+    height: auto;
   }
-  .swiper-button-next{
-    right: 5px;
+
+  .swiper-button-next,
+  .swiper-button-prev {
+    display: none;
   }
-  .swiper-button-prev{
-    left: 5px;
-  }
-</style>
+}
+
+.swiper-button-next.swiper-button-disabled,
+.swiper-button-prev.swiper-button-disabled {
+  opacity: 0 !important;
+}
+
+.swiper-button-next {
+  right: 5px;
+}
+
+.swiper-button-prev {
+  left: 5px;
+}</style>
