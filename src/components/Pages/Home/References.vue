9593bffb644e7de8b64372254182877e2267f4f9
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-06-22 09:23:04 +0100

    made reference dynamic

--- src/components/Pages/Home/References.vue
+++ src/components/Pages/Home/References.vue
@@ -5,19 +5,11 @@
         </div>
         <div class="row container-fluid">
             <ul class="nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" data-bs-toggle="pill" href="#ex3-1" role="tab">
-                        Mr. Sergiu Orbu
+                <li v-for="(referee, index) in referees" :key="index" class="nav-item">
+                    <a class="nav-link" :class="{ 'active': index === activeReferee }" data-bs-toggle="pill"
+                        :href="'#referee-' + (index + 1)" role="tab" @click="changeActiveReferee(index)">
+                        {{ referee.name }}
                     </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="pill" href="#ex3-2" role="tab">
-                        Dr. Edel Sheratt
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="pill" href="#ex3-3" role="tab" aria-controls="ex3-3">Dr. Syed
-                        Azeem</a>
                 </li>
             </ul>
 
@@ -29,20 +21,48 @@
 
             <div class="row">
                 <div class="col-2 open-quote"><i class="fa-solid fa-quote-left"></i></div>
-                <div class="col-8" data-aos="zoom-in">
+                <div class="col-8" data-aos="zoom-in" style="padding: 20px 0px;">
                     <div class="tab-content">
-                        <div class="tab-pane fade show active" id="ex3-1" role="tabpanel">
+                        <div v-for="(referee, index) in referees" :key="index" class="tab-pane fade"
+                            :class="{ 'show active': index === activeReferee }" :id="'referee-' + (index + 1)"
+                            role="tabpanel">
                             <kinesis-container>
                                 <kinesis-element :strength="5" type="depth">
-                                    a 
+                                    <blockquote>
+                                        {{ referee.description }}
+                                    </blockquote>
+                                    <div class="row">
+                                        <div class="col-6 line" style="text-align: end;">--------------</div>
+                                        <div class="col-xx-6 col-lg-6 col-md-12">
+                                            <p>
+                                                {{ referee.name }}
+                                                <br />
+                                                {{ referee.position }}
+                                            </p>
+                                            <p style="text-align: end;">
+                                                <img style="border-radius: 50%;border: 2px solid white; width: 150px;"
+                                                    :src="referee.image" />
+                                                <br />
+                                                <a :href="referee.linkedin" target="_blank">
+                                                    <!-- Linkedin -->
+                                                    <i class="fa-brands fa-linkedin"
+                                                        style="padding: 10px; font-size: 30px;"></i>
+                                                </a>
+                                                <a :href="referee.telephone">
+                                                    <!-- Phone -->
+                                                    <i class="fa-solid fa-phone"
+                                                        style="padding: 10px; font-size: 30px;"></i>
+                                                </a>
+                                                <a :href="referee.email">
+                                                    <!-- Email -->
+                                                    <i class="fa-solid fa-envelope"
+                                                        style="padding: 10px; font-size: 30px;"></i>
+                                                </a>
+                                            </p>
+                                        </div>
+                                    </div>
                                 </kinesis-element>
                             </kinesis-container>
-                        </div>
-                        <div class="tab-pane fade" id="ex3-2" role="tabpanel">
-                            b 
-                        </div>
-                        <div class="tab-pane fade" id="ex3-3" role="tabpanel">
-                            c 
                         </div>
                     </div>
                 </div>
@@ -58,11 +78,45 @@
         </div>
     </div>
 </template>
-  
+
 <script>
 export default {
-}
+    data() {
+        return {
+            referees: [
+                {
+                    name: 'Mr. Sergiu Orbu',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit est molestiae aperiam aliquid illum in, deserunt vero quidem facilis, id, libero voluptatibus possimus. Impedit aliquam culpa qui beatae dolore dicta?',
+                    position: 'Head of Web Development, Net World Sports',
+                    image: 'https://t4.ftcdn.net/jpg/02/22/39/63/360_F_222396357_KlP0TQwV3X1U6rJWzlLcIpJ7ZLpxGcQR.jpg',
+                    linkedin:'https://google.co.uk',
+                    telephone:'tel:123-456-7890',
+                    email:'mailto:dabir.rizvi@gmail.com'
+                },
+                {
+                    name: 'Dr. Edel Sheratt',
+                    description: 'Description for Dr. Edel Sheratt',
+                    position: 'Position for Dr. Edel Sheratt',
+                    image: 'https://t4.ftcdn.net/jpg/02/22/39/63/360_F_222396357_KlP0TQwV3X1U6rJWzlLcIpJ7ZLpxGcQR.jpg'
+                },
+                {
+                    name: 'Dr. Syed Azeem',
+                    description: 'Description for Dr. Syed Azeem',
+                    position: 'Position for Dr. Syed Azeem',
+                    image: 'https://t4.ftcdn.net/jpg/02/22/39/63/360_F_222396357_KlP0TQwV3X1U6rJWzlLcIpJ7ZLpxGcQR.jpg'
+                }
+            ],
+            activeReferee: 0
+        };
+    },
+    methods: {
+        changeActiveReferee(index) {
+            this.activeReferee = index;
+        }
+    }
+};
 </script>
+
   
 <style lang="scss" scoped>
 .references {
@@ -100,15 +154,35 @@ hr {
 
     border-top: 10px dashed #8c8b8b;
 }
+
 // replacing bootstrap navbar
 .nav-link {
     color: #003049;
-    &:hover{
+
+    &:hover {
         color: #8c8b8b;
     }
-  &.active {
-    background-color: white !important;
-    color: black !important;
-  }
+
+    &.active {
+        background-color: white !important;
+        color: black !important;
+    }
+}
+
+.line {
+    text-align: end;
+}
+
+@media screen and (max-width: 990px) {
+    .line {
+        display: none;
+    }
+}
+
+.col-6 {
+    padding: 0;
+}
+a{
+    color: #fefae0;
 }
 </style>
