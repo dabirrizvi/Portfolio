0b989d16974a10bff78c9d489796ae7116a751e5
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-06-23 11:23:04 +0100

    removed dependency

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import copy from 'rollup-plugin-copy';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    copy({
-      targets: [
-        { src: '_redirects', dest: 'dist' }
-      ]
-    })
-  ]
-});
+  plugins: [vue()],
+})
