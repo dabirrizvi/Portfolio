99cb7c9f2d1fbd37dc4347f7f1255530a4960bb2
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-06-23 11:16:57 +0100

    importing redirect

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import copy from 'rollup-plugin-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    copy({
+      targets: [
+        { src: '_redirects', dest: 'dist' }
+      ]
+    })
+  ]
+});
