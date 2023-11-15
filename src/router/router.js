53dceb365f931c33b66f830ea0792556795d9370
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-15 01:08:50 +0000

    added routing back

--- src/router/router.js
+++ src/router/router.js
@@ -6,15 +6,13 @@ import Projects from '../components/Pages/Projects/Project.vue'
 import ContactUs from '../components/Pages/Contacts/Contact.vue'
 import NotFound from '../components/Pages/404.vue'
 
-
-
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: LaunchScreen },
-        // { path: '/home', component: Home },
-        // { path: '/projects', component: Projects},
-        // { path: '/contact', component: ContactUs},
+        { path: '/home', component: Home },
+        { path: '/projects', component: Projects},
+        { path: '/contact', component: ContactUs},
         { path: '/:notFound(.*)', component: NotFound},
     ],
 });
