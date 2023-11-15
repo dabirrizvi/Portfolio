77d0afd05879dd364a7b446cb916997275fa0bf0
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-15 01:07:14 +0000

    added new intro

--- src/components/Pages/Home/Intro.vue
+++ src/components/Pages/Home/Intro.vue
@@ -1,60 +1,64 @@
 <template>
   <div class="container content row" data-aos="fade-left">
-    <div class="col-xxl-4 col-lg-4 col-md-0 extra-spacing" style="margin: 0;"></div>
-    <div class="col-xxl-8 col-lg-8 col-md-12 content-intro">
-      <h1 class="intro-mobile">Hi There !</h1>
-      <h2>My Name is Dabir Hasan Rizvi</h2>
-      <p>I am a passionate software developer, driven by the desire to create innovative solutions and simplify complex
-        problems. With a solid foundation in programming languages and a strong problem-solving mindset, I strive to craft
-        efficient and scalable software solutions that empower businesses and make a positive impact.</p>
-      <button type="button" class="btn btn-outline-secondary">Download CV <i
-          class="fa-solid fa-file-arrow-down"></i></button>
+    <div class="col-1">
       <section>
-        <!-- Facebook -->
-        <i class="fa-brands fa-facebook"></i>
-        <!-- Instagram -->
-        <i class="fa-brands fa-instagram"></i>
         <!-- Linkedin -->
         <i class="fa-brands fa-linkedin"></i>
+        <br />
         <!-- Github -->
         <i class="fa-brands fa-github"></i>
+        <br />
         <!-- Phone -->
         <i class="fa-solid fa-phone"></i>
+        <br />
         <!-- Email -->
         <i class="fa-solid fa-envelope"></i>
+        <br />
+        <!-- Facebook -->
+        <i class="fa-brands fa-facebook"></i>
+        <br />
+        <!-- Instagram -->
+        <i class="fa-brands fa-instagram"></i>
+        <br />
       </section>
+    </div>
+    <div class="col-11">
+      <div class="heading-intro">
+        <h2>Hi! My Name is Dabir Hasan Rizvi.</h2>
+      </div>
+      <div class="intro-content">
+        <p>
+          Welcome to my portfolio! I'm a result-driven Software Developer with a diverse skill set that encompasses
+          web-based applications, as well as artificial intelligence and machine learning. With a strong academic
+          foundation
+          and over a year of hands-on experience in agile teams, I'm dedicated to bringing creativity and efficiency to
+          the
+          world of software development. My journey has led me to become proficient in object-oriented programming, with a
+          strong emphasis on AI and ML applications. I'm technologically savvy, and my unwavering drive to excel fuels my
+          ambition to contribute effectively to any organisation's success. Explore my portfolio to discover the projects
+          that reflect my dedication to the ever-evolving fields of software development and AI/ML.
+        </p>
+      </div>
+
+      <button type="button" class="btn btn-outline-secondary">Download CV <i
+          class="fa-solid fa-file-arrow-down"></i></button>
     </div>
   </div>
 </template>
 <style scoped>
 .content {
-  min-height: 100vh;
   display: flex;
   overflow: hidden;
   margin: auto;
-  padding: 20px;
-}
-
-@media (min-width: 990px) {
-  .intro-mobile {
-    display: none;
-  }
-
-  .content {
-    align-items: center;
-  }
-}
-
-@media (max-width: 990px) {
-  .extra-spacing {
-    display: none;
-  }
+  max-width: 1200px;
+  margin: auto;
+  padding: 0;
 }
 
 h1,
 h2 {
   font-size: 2rem;
-  color: #FFE4C4;
+  color: #fff;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
 
@@ -62,7 +66,7 @@ h2 {
 
 p {
   font-size: 1.2rem;
-  color: #FFE4C4;
+  color: #fff;
   text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
   font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
 
@@ -80,9 +84,11 @@ p {
   background-color: rgba(255, 255, 255, 0.3);
   border-color: #FFE4C4;
 }
-.fa-brands, .fa-solid {
-  font-size: 1.5rem;
-  padding: 30px 20px 20px 0px;
+
+.fa-brands,
+.fa-solid {
+  font-size: 2rem;
+  padding: 2rem 0px;
   color: #FFE4C4;
 }
 </style>
