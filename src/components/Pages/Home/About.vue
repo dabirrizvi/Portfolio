9a67f7df9711bf7473f3f783c1872d58bdcb9a9f
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-18 22:31:51 +0000

    about section styling

--- src/components/Pages/Home/About.vue
+++ src/components/Pages/Home/About.vue
@@ -3,6 +3,7 @@
         <div class="col-xxl-5 col-lg-5 col-md-0 extra-spacing" style="margin: 0;"></div>
         <div class="col-xxl-7 col-lg-7 col-md-12 content-intro">
                 <h3>About Me</h3>
+                <section class="about-content">
                 <p>
                     I thrive at the intersection of creativity and data-driven problem-solving, where technology becomes a
                     powerful tool for innovation. My academic journey has provided me with a robust foundation in computer
@@ -28,6 +29,13 @@
                     my journey. Whether you're interested in collaboration, have an exciting project idea, or simply want to
                     connect with a passionate software developer and AI/ML enthusiast, don't hesitate to reach out.
                 </p>
+            </section>
+                <p class="img-content">
+                    <img class="img-logo" src="https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Timeline/no_bg/aber.png"/>
+                    <img class="img-logo" src="https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Timeline/no_bg/cmr.png"/>
+                    <img class="img-logo" src="https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Timeline/no_bg/rnr.png"/>
+                    <img class="img-logo" src="https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Timeline/no_bg/nws.png"/>
+                </p>
             </div>
     </div>
 </template>
@@ -47,21 +55,44 @@
     z-index: -999;
     min-height: 100vh;
 }
+@media (max-width:1400px){
+    .content-about {
+        background-image:
+        linear-gradient(to top,
+            rgba(51, 65, 85, 0.9),
+            rgba(51, 65, 85, 0.9)),
+            url('https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Background/dabir_mobile.jpg');
+
+    }
+}
 .content-intro{
     margin: auto;
+    padding: 100px 20px;
 }
 p,
 h3 {
-    color: #FFE4C4;
+    color: #f8f9fa;
     text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
-    font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
-    padding: 50px;
+    padding: 10px;
 }
-
+.about-content{
+    padding: 100px 0px;
+}
 p {
-    font-size: 1.2rem;
+    font-size: 1rem;
+    font-family: 'Roboto', sans-serif;
+
 }
 
 h3 {
     text-align: center;
-}</style>
+    font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
+}
+.img-content{
+    display: flex;
+  justify-content: space-evenly; 
+}
+.img-logo{
+    width: 20%;
+}
+</style>
