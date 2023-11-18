6fe57750af5c132876157184ed6ba1876be0cc95
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-18 23:01:44 +0000

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      '/.netlify/functions': {
+        target: 'http://localhost:8888', 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/\.netlify\/functions/, ''),
+      },
+    },
+  },
+});
