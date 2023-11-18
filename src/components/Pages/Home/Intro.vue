1e835e09175c4530221d0514b99eb0e9c7be303b
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-18 14:11:32 +0000

    styling intro section

--- src/components/Pages/Home/Intro.vue
+++ src/components/Pages/Home/Intro.vue
@@ -17,33 +17,31 @@
           that reflect my dedication to the ever-evolving fields of software development and AI/ML.
         </p>
       </div>
-
-      <button type="button" class="btn btn-outline-secondary">Download CV <i
+      <button type="button" class="btn">Download CV <i
           class="fa-solid fa-file-arrow-down"></i></button>
     </div>
     <div class="col-xxl-1 col-md-12"></div>
-
     <div class="col-xxl-1 col-md-12">
       <ul>
         <li>
           <!-- Linkedin -->
-          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-linkedin fa-socials"></i>
         </li>
         <li>
           <!-- Github -->
-          <i class="fa-brands fa-github"></i>
+          <i class="fa-brands fa-github fa-socials"></i>
         </li>
         <li>
           <!-- Phone -->
-          <i class="fa-solid fa-phone"></i>
+          <i class="fa-solid fa-phone fa-socials"></i>
         </li>
         <li>
           <!-- Email -->
-          <i class="fa-solid fa-envelope"></i>
+          <i class="fa-solid fa-envelope fa-socials"></i>
         </li>
         <li>
           <!-- Instagram -->
-          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-instagram fa-socials"></i>
         </li>
       </ul>
     </div>
@@ -61,39 +59,77 @@
   padding: 0;
   align-items: center;
 }
+@media(max-width: 1200px) {
+  .content {
+    padding-left: 10px;
+  }
+}
 .heading-intro > h2{
   font-size: 2rem;
   color: #fff;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
-
+  padding: 0px 10px 50px 0px;
 }
 
 .intro-content >p {
-  font-size: 1.2rem;
+  font-size: 1rem;
   color: #fff;
   text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+  font-family: 'Roboto', sans-serif;
+text-align: justify;
+}
+
+/* Button styling */
+.btn {
+  margin: 50px 0px;
+  position: relative;
+  overflow: hidden;
+  background-color: rgba(255, 255, 255, 0.4);
+  border: 1px solid #333;
+  border-radius: 25px;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 1.5rem;
+  cursor: pointer;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+  transition: color 0.5s ease-in-out, background-color 0.5s ease-in-out; 
   font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
 
 }
 
-.btn {
-  font-size: 1.5rem;
-  color: #FFE4C4;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-color: #FFE4C4;
-  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+.btn::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 100%;
+  width: 100%;
+  height: 100%;
+background-color: rgba(255, 228, 196, 0.4);
+  transition: right 0.5s ease-in-out;
+  z-index: -1;
+}
+
+.btn:hover::before {
+  right: 0;
 }
 
 .btn:hover {
-  background-color: rgba(255, 255, 255, 0.3);
-  border-color: #FFE4C4;
+  color:  rgba(255, 228, 196);
+;
+
 }
 
-.fa-brands,
-.fa-solid {
+.fa-file-arrow-down{
   font-size: 2rem;
-  color: #FFE4C4;
+  margin-left: 10px;
+}
+
+/* Socials styling */
+
+.fa-socials{
+  font-size: 2rem;
+  color: #fff;
 }
 ul {
   list-style: none;
@@ -105,6 +141,12 @@ ul {
 li{
   padding: 2rem 0;
 }
+
+li:hover .fa-brands,
+li:hover .fa-solid {
+  color: #FFE4C4; 
+}
+
 
 @media (max-width: 1400px) {
   ul{
