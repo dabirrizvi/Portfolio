7c6a7dff0fd349fb62a8db925405b5fb7ca8c826
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-20 14:30:54 +0000

    making timeline to html inputs

--- src/components/UI/PageTemplate/Timeline.vue
+++ src/components/UI/PageTemplate/Timeline.vue
@@ -2,30 +2,32 @@
   <div>
     <div class="cd-vertical-timeline">
       <div class="cd-line"></div>
-      <h2 class="title">{{ title }}</h2>
+      <h2 class="title" v-html="title"></h2>
+
       <div v-for="(event, index) in events" :key="index" class="cd-timeline-block">
         <div class="cd-timeline-img">
           <img :src="event.image" alt="Event Image" />
         </div>
         <div class="cd-timeline-content" data-aos="fade-up">
-          <h2 class="sub-heading">{{ event.name }}</h2>
-          <p>{{ event.date }}</p>
-          <h2 class="sub-heading">{{ event.title }}</h2>
-          <p>{{ event.description }}</p>
+          <h2 class="sub-heading" v-html="event.name"></h2>
+          <p v-html="event.date"></p>
+          <h2 class="sub-heading" v-html="event.title"></h2>
+          <p v-html="event.description"></p>
           <!-- Bootstrap accordion -->
-          <div v-if="event.list && event.list.length" :id="'accordionFlushExample' + event.id" class="accordion accordion-flush">
+          <div v-if="event.list && event.list.length" :id="'accordionFlushExample' + event.id"
+            class="accordion accordion-flush">
             <div class="accordion-item">
               <h2 class="accordion-header" :id="'flush-headingOne' + event.id">
-                <button class="accordion-button collapsed" type="button" :data-bs-toggle="'collapse'" :data-bs-target="'#flush-collapseOne' + event.id"
-                  :aria-expanded="false" :aria-controls="'flush-collapseOne' + event.id">
-                  {{ event.button_text }}
+                <button class="accordion-button collapsed" type="button" :data-bs-toggle="'collapse'"
+                  :data-bs-target="'#flush-collapseOne' + event.id" :aria-expanded="false"
+                  :aria-controls="'flush-collapseOne' + event.id" v-html="event.button_text">
                 </button>
               </h2>
-              <div :id="'flush-collapseOne' + event.id" class="accordion-collapse collapse" :aria-labelledby="'flush-headingOne' + event.id"
-                :data-bs-parent="'#accordionFlushExample' + event.id">
+              <div :id="'flush-collapseOne' + event.id" class="accordion-collapse collapse"
+                :aria-labelledby="'flush-headingOne' + event.id" :data-bs-parent="'#accordionFlushExample' + event.id">
                 <div class="accordion-body">
                   <ul>
-                    <li v-for="(item, itemIndex) in event.list" :key="itemIndex">{{ item }}</li>
+                    <li v-for="(item, itemIndex) in event.list" :key="itemIndex" v-html="item"></li>
                   </ul>
                 </div>
               </div>
@@ -119,7 +121,7 @@ export default {
   margin-left: 5rem;
   color: #323d4f;
   font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
-font-size: 2rem;
+  font-size: 2rem;
 }
 
 @media (max-width: 990px) {
@@ -143,13 +145,13 @@ h1 {
 li {
   font-size: 1rem;
   line-height: 30px;
-  color: rgba(50,61,79);
+  color: rgba(50, 61, 79);
   font-family: 'Roboto', sans-serif;
-  }
+}
 
 
-  // Custom accordion styling
-  /* Custom styles for Bootstrap accordion */
+// Custom accordion styling
+/* Custom styles for Bootstrap accordion */
 .accordion {
   border-top-right-radius: 25px;
   border-top-left-radius: 25px;
@@ -157,21 +159,22 @@ li {
 }
 
 .accordion-item {
-  background-color: rgba(50,61,79,0.15);
+  background-color: rgba(50, 61, 79, 0.15);
   border-radius: 8px;
   margin-bottom: 10px;
 }
 
 
 /* Remove default Bootstrap button icon */
-.accordion{
+.accordion {
   --bs-accordion-btn-icon: none;
-  --bs-accordion-btn-active-icon:none
+  --bs-accordion-btn-active-icon: none
 }
+
 .accordion-button {
-  background-color: rgba(50,61,79,0.8);
+  background-color: rgba(50, 61, 79, 0.8);
   color: #f8f9fa;
-  position: relative; 
+  position: relative;
 }
 
 .accordion-button::after {
@@ -180,19 +183,20 @@ li {
   font-weight: 900;
   display: inline-block;
   position: relative;
-  transform-origin:left; /* Set the transform origin to the right */
-  transition: transform 0.2s ease-in-out; /* Add a transition for a smooth effect */
+  transform-origin: left;
+  /* Set the transform origin to the right */
+  transition: transform 0.2s ease-in-out;
+  /* Add a transition for a smooth effect */
 }
-.accordion-button:focus{
+
+.accordion-button:focus {
   box-shadow: none;
 }
 
 .accordion-button:hover {
-  background-color: rgba(50,61,79,1);
+  background-color: rgba(50, 61, 79, 1);
 }
 
 .accordion-body {
   padding: 15px;
-}
-
-</style>
+}</style>
