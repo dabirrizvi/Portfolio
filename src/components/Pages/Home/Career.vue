cab95fe049489bb85a284d281af7708449cdbe32
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-20 14:30:21 +0000

    dynamic career section with links

--- src/components/Pages/Home/Career.vue
+++ src/components/Pages/Home/Career.vue
@@ -13,9 +13,9 @@ export default {
       educationEvents: [
         {
           id: '1',
-          name: 'Aberystwyth University ',
+          name: '<a style="text-decoration:none; color:inherit;" target=_blank href="https://www.aber.ac.uk/en/">Aberystwyth University</a>',
           date: 'September 2021 - September 2023',
-          title: 'MSc. Advanced Computer Science (with Integrated Year in Industry)',
+          title: '<a style="text-decoration:none; color:inherit;" target=_blank href="https://courses.aber.ac.uk/postgraduate/software-engineering-integrated-industrial-year/">MSc. Advanced Computer Science (with Integrated Year in Industry)</a>',
           image: 'https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Timeline/aber.jpg',
            description: 'Distinction (Expected)',
           button_text: 'Relevant modules',
@@ -23,9 +23,9 @@ export default {
         },
         {
           id: '2',
-          name: 'CMR Institute of Technology',
+          name: '<a style="text-decoration:none; color:inherit;" target=_blank href="https://www.cmrit.ac.in/">CMR Institute of Technology</a>',
           date: 'August 2016 - August 2020',
-          title: 'B.E in Electronics & Communication Engineering',
+          title: '<a style="text-decoration:none; color:inherit;" target=_blank href="https://www.cmrit.ac.in/courses/be-electronics-communication-engineering/">B.E in Electronics & Communication Engineering</a>',
           image: 'https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Timeline/cmr.jpg',
            description: 'Second Class Hons.',
           button_text: 'Relevant Modules',
@@ -35,22 +35,22 @@ export default {
       workExperienceEvents: [
         {
           id: '3',
-          name: 'Net World Sports Ltd.',
+          name: '<a style="text-decoration:none; color:inherit;" target=_blank href="https://www.networldsports.co.uk/about">Net World Sports Ltd.</a>',
           date: 'July 2022 - June 2023',
           title: 'Junior Web Developer',
           image: 'https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Timeline/nws.jpg',
           description: 'Full-Time (Industrial Year)',
           button_text: 'View Details',
            list: ['Built responsive and reusable front-end components using a micro-service architecture with Vue.js.', 
-           'Implemented responsive design experience and ensured optimal user experience for the company’s blog website working in collaboration with the content and SEO teams.',
+           'Implemented responsive design experience and ensured optimal user experience for the company’s blog website working in collaboration with the content and SEO teams <a style="text-decoration:none; color:inherit;" target=_blank href="https://blog.networldsports.co.uk/"><i class="fa-solid fa-right-from-bracket"></i></a>',
             'Identified workflow pain points across all departments and developed a Chrome Extension, working with APIs to address those needs.',
-           'Employing Agile Methodology to migrate 40+ e-commerce stores from Magento 1 to Magento 2, improving website performance and customer experience.',
+           'Employed Agile Methodology to migrate 40+ e-commerce stores from Magento 1 to Magento 2, improving website performance and customer experience.',
            'Tools and languages used: JavaScript, Php, jQuery, Manifest JS, Vue.js, Magento 2, WordPress, HTML, CSS, Sass.'
            ],
         },
         {
           id: '4',
-          name: 'Racks & Rollers | Storage Technologies & Automation',
+          name: '<a style="text-decoration:none; color:inherit;" target=_blank href="https://racksandrollers.com/">Racks & Rollers | Storage Technologies & Automation</a>',
           date: 'January 2020 - July 2020',
           title: 'Java Programmer - Internship',
           image: 'https://raw.githubusercontent.com/dabirrizvi/assets_for_portfolio/main/Timeline/rnr.jpg',
