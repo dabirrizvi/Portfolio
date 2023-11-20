0914e1c5443513c85c2a1548e476db7ebffb1708
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-20 01:52:25 +0000

    styling

--- src/components/Pages/Home/Intro.vue
+++ src/components/Pages/Home/Intro.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container content row" data-aos="fade-left">
+  <div class="container content row" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
     <div class="col-xxl-10 col-md-12" style="padding-left: 0px;">
       <div class="heading-intro">
-        <h2 >Hi! My Name is Dabir Hasan Rizvi.</h2>
+        <h2>Hi! My Name is Dabir Hasan Rizvi.</h2>
       </div>
       <div class="intro-content">
         <p>
@@ -16,35 +16,69 @@
           ambition to contribute effectively to any organisation's success. Explore my portfolio to discover the projects
           that reflect my dedication to the ever-evolving fields of software development and AI/ML.
         </p>
-      </div>
-  <a href="https://github.com/dabirrizvi/assets_for_portfolio/raw/main/CV/CV.pdf" download="cv.pdf">
-    <button type="button" class="btn">Download CV <i class="fa-solid fa-file-arrow-down"></i></button>
-  </a>
+      </div>      
+      <kinesis-container>
+        <kinesis-element :strength="5" type="translate">
+          <a href="https://github.com/dabirrizvi/assets_for_portfolio/raw/main/CV/CV.pdf" download="cv.pdf">
+            <button type="button" class="btn">Download CV <i class="fa-solid fa-file-arrow-down"></i></button>
+          </a>
+        </kinesis-element>
+      </kinesis-container>
 
- </div>
+    </div>
     <div class="col-xxl-1 col-md-12"></div>
     <div class="col-xxl-1 col-md-12">
       <ul>
         <li>
-          <!-- Linkedin -->
-          <a target="_blank" href="https://www.linkedin.com/in/dabir-hasan-rizvi-738a83185/"><i class="fa-brands fa-linkedin fa-socials"></i></a>
+          <kinesis-container>
+            <kinesis-element :strength="10" type="depth">
+              <!-- Linkedin -->
+              <a target="_blank" href="https://www.linkedin.com/in/dabir-hasan-rizvi-738a83185/"><i
+                  class="fa-brands fa-linkedin fa-socials"></i></a>
+            </kinesis-element>
+          </kinesis-container>
+
         </li>
-        
+
         <li>
-          <!-- Github -->
-          <a target="_blank" href="https://github.com/dabirrizvi"><i class="fa-brands fa-github fa-socials"></i></a>
+          <kinesis-container>
+            <kinesis-element :strength="10" type="depth">
+              <!-- Github -->
+              <a target="_blank" href="https://github.com/dabirrizvi"><i class="fa-brands fa-github fa-socials"></i></a>
+            </kinesis-element>
+          </kinesis-container>
+
+
         </li>
         <li>
+          <kinesis-container>
+            <kinesis-element :strength="10" type="depth">
           <!-- Phone -->
           <a href="tel:+44(0)7737906374"><i class="fa-solid fa-phone fa-socials"></i></a>
+            </kinesis-element>
+          </kinesis-container>
+
+
         </li>
         <li>
+          <kinesis-container>
+            <kinesis-element :strength="10" type="depth">
           <!-- Email -->
-          <a href="mailto:dabir.rizvi@gmail.com?subject=General%20Inquiry&body=Hi%20Dabir,"><i class="fa-solid fa-envelope fa-socials"></i></a>
+          <a href="mailto:dabir.rizvi@gmail.com?subject=General%20Inquiry&body=Hi%20Dabir,"><i
+              class="fa-solid fa-envelope fa-socials"></i></a>
+            </kinesis-element>
+          </kinesis-container>
+
         </li>
         <li>
+          <kinesis-container>
+            <kinesis-element :strength="10" type="depth">
           <!-- Instagram -->
-          <a target="_blank" href="https://www.instagram.com/dabirrizvi/"><i class="fa-brands fa-instagram fa-socials"></i></a>
+          <a target="_blank" href="https://www.instagram.com/dabirrizvi/"><i
+              class="fa-brands fa-instagram fa-socials"></i></a>
+            </kinesis-element>
+          </kinesis-container>
+
         </li>
       </ul>
     </div>
@@ -52,7 +86,7 @@
 </template>
 
 <style scoped>
-.content { 
+.content {
   display: flex;
   overflow: hidden;
   margin: auto;
@@ -62,25 +96,27 @@
   padding: 0;
   align-items: center;
 }
+
 @media(max-width: 1200px) {
   .content {
     padding-left: 10px;
   }
 }
-.heading-intro > h2{
+
+.heading-intro>h2 {
+  
   font-size: 2rem;
-  color: #f8f9fa;
+  color: #FFE4C4;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
   padding: 0px 10px 50px 0px;
 }
 
-.intro-content >p {
-  font-size: 1rem;
+.intro-content>p {
+  font-size: 1.5rem;
   color: #f8f9fa;
   text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
   font-family: 'Roboto', sans-serif;
-text-align: justify;
 }
 
 /* Button styling */
@@ -96,9 +132,8 @@ text-align: justify;
   font-size: 1.5rem;
   cursor: pointer;
   text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
-  transition: color 0.5s ease-in-out, background-color 0.5s ease-in-out; 
+  transition: color 0.5s ease-in-out, background-color 0.5s ease-in-out;
   font-family: 'Space Mono', monospace, Arial, Helvetica, sans-serif;
-
 }
 
 .btn::before {
@@ -108,7 +143,7 @@ text-align: justify;
   right: 100%;
   width: 100%;
   height: 100%;
-background-color: rgba(255, 228, 196, 0.4);
+  background-color: rgba(255, 228, 196, 0.4);
   transition: right 0.5s ease-in-out;
   z-index: -1;
 }
@@ -118,44 +153,46 @@ background-color: rgba(255, 228, 196, 0.4);
 }
 
 .btn:hover {
-  color:  rgba(255, 228, 196);
-;
+  color: rgba(255, 228, 196);
+  ;
 
 }
 
-.fa-file-arrow-down{
+.fa-file-arrow-down {
   font-size: 2rem;
   margin-left: 10px;
 }
 
 /* Socials styling */
 
-.fa-socials{
+.fa-socials {
   font-size: 2rem;
   color: #f8f9fa;
 }
+
 ul {
   list-style: none;
   padding: 0;
   display: flex;
-  flex-direction: column; /* Display list items vertically */
-  justify-content: space-evenly; /* Space items evenly */
+  flex-direction: column;
+  /* Display list items vertically */
+  justify-content: space-evenly;
+  /* Space items evenly */
 }
-li{
+
+li {
   padding: 2rem 0;
 }
 
 li:hover .fa-brands,
 li:hover .fa-solid {
-  color: #FFE4C4; 
+  color: #FFE4C4;
 }
 
 
 @media (max-width: 1400px) {
-  ul{
+  ul {
     flex-direction: row;
   }
 }
-
 </style>
-<script></script>
