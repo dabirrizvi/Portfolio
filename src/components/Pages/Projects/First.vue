cb010e31bcc2501f4477beabddbd5120ddf32588
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2023-11-28 21:25:09 +0000

    removed animation for project intro

--- src/components/Pages/Projects/First.vue
+++ src/components/Pages/Projects/First.vue
@@ -2,22 +2,22 @@
   <div class="background">
     <div class="row content-container intro-content-container" >
       <h2 data-aos="fade-in">Embarking on a Technological Odyssey</h2>
-        <p class="intro-content" data-aos="fade-left">Navigating the dynamic intersection of technological ingenuity and hands-on
+        <p class="intro-content" >Navigating the dynamic intersection of technological ingenuity and hands-on
           problem-solving, my journey unfolds in the vibrant landscape where theory meets practice. Rooted in a robust
           academic foundation in computer science, I've ventured beyond textbooks, delving into the practical arena of
           software development with remarkable agility.</p>
-        <p class="intro-content" data-aos="fade-right">My professional odyssey thrives in the collaborative heartbeat of agile teams.
+        <p class="intro-content" >My professional odyssey thrives in the collaborative heartbeat of agile teams.
           Adaptability, problem-solving, and the art of delivering tangible solutions define my everyday ethos.
           Each project becomes a canvas for applying academic insights to real-world challenges, sculpting them into
           impactful solutions.</p>
-        <p class="intro-content" data-aos="fade-left">At the core of my endeavors lies an unyielding passion for innovation in artificial
+        <p class="intro-content" >At the core of my endeavors lies an unyielding passion for innovation in artificial
           intelligence and machine learning. The captivating allure of these technologies inspires me to navigate
           uncharted territories, be it unraveling the mysteries of computer vision, mastering the nuances of natural
           language processing, or sculpting predictive models that resonate with real-world needs.</p>
-        <p class="intro-content" data-aos="fade-right">My enthusiasm for technology transcends the theoretical, finding expression in each
+        <p class="intro-content" >My enthusiasm for technology transcends the theoretical, finding expression in each
           project I undertake. Whether crafting scalable web applications or sculpting intricate machine learning models,
           I approach every challenge with zest and a commitment to transformative outcomes.</p>
-        <p class="intro-content" data-aos="fade-left">Embark on a journey through my portfolio to witness the practical dimensions of my
+        <p class="intro-content" >Embark on a journey through my portfolio to witness the practical dimensions of my
           experience. It's a testament to the diverse projects that have defined my path and showcased the real-world
           impact of my skills. Whether you're seeking collaboration, have an exciting project in mind, or simply wish to
           connect with a practical-minded technologist, I'm here to explore the boundless possibilities with you.</p>
