ed9cf44a4c4ccf1455ca1f0b383f7afd3ecad44b
Dabir Hasan Rizvi <dabir@networldsports.co.uk>
2025-06-23 23:51:48 +0100

    Updated screen

--- src/components/UI/LoadingScreen/LaunchScreen.vue
+++ src/components/UI/LoadingScreen/LaunchScreen.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="loader" class="flex-column">
     <h2 class="title">Dabir Hasan Rizvi</h2>
-    <p class="subtitle">Software Developer</p>
+    <p class="subtitle">Software Engineer</p>
     <div class="svg">
       <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
         x="0px" y="0px" viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve">
